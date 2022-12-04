9d6fb2910eeb4b62884cc6558647a42a48b09f70
David Andersson <davidandersson99@gmail.com>
2022-12-04 12:55:30 +0100

    Removed print and updated comments

--- 4/part2.py
+++ 4/part2.py
@@ -2,11 +2,7 @@ f = open("input.txt")
 
 lines = f.readlines()
 
-def test_data():
-    return lines[1] + lines[2] + lines[3] + lines[4] + lines[5]
-
 def is_contained(src, trg):
-
     src_split = src.split("-")
     src_bottom = int(src_split[0])
     src_top = int(src_split[1])
@@ -23,7 +19,6 @@ def is_contained(src, trg):
     return True
 
 def solve(lines):
-    print("size of data:", len(lines))
     counter = 0
     for line in lines:
         ranges = line.split(",")
@@ -32,7 +27,7 @@ def solve(lines):
         first = ranges[0]
         second = ranges[1]
 
-        #check if first range is fully contained in second
+        #check if range is contained
         if is_contained(first, second):
             counter += 1
     print(counter)
