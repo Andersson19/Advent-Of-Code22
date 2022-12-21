7a0d64704309b786edf97c8f3cab8bfacff979a8
David Andersson <davidandersson99@gmail.com>
2022-12-21 10:14:29 +0100

    almost done with 8a, now just need to check all trees in all directions if visible or not

--- 8/8a.py
+++ 8/8a.py
@@ -23,7 +23,6 @@ class Tree:
     def __init__(self, height, coordinates, forest):
         self.height = height
         self.coordinates = coordinates
-        self.visible = False
         self.forest = forest
         self.left_height = 0
         self.right_height = 0
@@ -33,13 +32,21 @@ class Tree:
         self.right_visible = True if coordinates[1] == self.forest.cols - 1 else None
         self.top_visible = True if coordinates[0] == 0 else None
         self.bottom_visible = True if coordinates[0] == self.forest.rows - 1 else None
+        self.visible = self.left_visible or self.right_visible or self.top_visible or self.bottom_visible
 
     #get height of the tree
     def get_height(self):
         return self.height
     
     def __str__(self):
-        return "Tree at " + str(self.coordinates) + " with height " + str(self.height)
+        str = ""
+        if self.visible == None:
+            str += "   [None]    "
+        elif self.visible:
+            str += "  [Visible]  "
+        else:
+            str += "[Not visible]"
+        return str
 
 #class Forest with attributes:
 #   trees: 2d list of Tree objects, initally empty
@@ -52,10 +59,15 @@ class Forest:
         self.cols = cols
 
     def __str__(self):
-        return "Forest with " + str(self.rows) + " rows and " + str(self.cols) + " columns"
+        string = ""
+        for tree in self.trees:
+            for t in tree:
+                string += str(t)
+            string += "\n"
+        return string
 
     #function set the forest
-    def set_forest(self, trees):
+    def set_trees(self, trees):
         self.trees = trees
 
     #function to set the height of the trees on the left, right, top and bottom
@@ -74,79 +86,47 @@ class Forest:
     #recursive function to check for each tree in forest if it is visible from any direction
     #  if the direction visibility is True, then check if height of tree is larger than the height of the tree in that direction, if so, set visible to true for that tree
     #  if any direction visibility is None, then return recursive call to that direction
-    def check_tree(self, tree):
-        if tree.left_visible == True:
-            if tree.height > tree.left_height:
-                #print statement that tree is visible from that direction
-                print("Tree at " + str(tree.coordinates) + " with height " + str(tree.height) + " is visible from the left")
-                tree.left_visible = True
-            else:
-                tree.left_visible = False
-        elif tree.left_visible == None:
-            print("left is None! checking left tree..")
-            self.check_tree(self.trees[tree.coordinates[0]][tree.coordinates[1] - 1])
-            if tree.height > tree.left_height:
-                #print statement that tree is visible from that direction
-                print("Tree at " + str(tree.coordinates) + " with height " + str(tree.height) + " is visible from the left")
-                tree.left_visible = True
-            else:
-                tree.left_visible = False
+    def check_tree(self):
 
-        if tree.right_visible == True:
-            if tree.height > tree.right_height:
-                #print statement that tree is visible from that direction
-                print("Tree at " + str(tree.coordinates) + " with height " + str(tree.height) + " is visible from the right")
-                tree.right_visible = True
-            else:
-                tree.right_visible = False
-        elif tree.right_visible == None:
-            print("right is None! checking right tree..")
-            self.check_tree(self.trees[tree.coordinates[0]][tree.coordinates[1] + 1])
-            if tree.height > tree.right_height:
-                #print statement that tree is visible from that direction
-                print("Tree at " + str(tree.coordinates) + " with height " + str(tree.height) + " is visible from the right")
-                tree.right_visible = True
-            else:
-                tree.right_visible = False
+        for i in range(1, self.rows-1):
+            for j in range(1, self.cols-1):
 
-        if tree.top_visible == True:
-            if tree.height > tree.top_height:
-                #print statement that tree is visible from that direction
-                print("Tree at " + str(tree.coordinates) + " with height " + str(tree.height) + " is visible from the top")
-                tree.top_visible = True
-            else:
-                tree.top_visible = False
-        elif tree.top_visible == None:
-            print("top is None! checking top tree..")
-            self.check_tree(self.trees[tree.coordinates[0] - 1][tree.coordinates[1]])
-            if tree.height > tree.top_height:
-                #print statement that tree is visible from that direction
-                print("Tree at " + str(tree.coordinates) + " with height " + str(tree.height) + " is visible from the top")
-                tree.top_visible = True
-            else:
-                tree.top_visible = False
+                if self.trees[i-1][j].visible == True:
+                    print("if " + str(self.trees[i][j].height) + " > " + str(self.trees[i-1][j].height))
+                    if self.trees[i][j].height > self.trees[i-1][j].height:
+                        print("found left visible")
+                        self.trees[i][j].left_visible = True
+                    else:
+                        self.trees[i][j].left_visible = False
 
-        if tree.bottom_visible == True:
-            if tree.height > tree.bottom_height:
-                #print statement that tree is visible from that direction
-                print("Tree at " + str(tree.coordinates) + " with height " + str(tree.height) + " is visible from the bottom")
-                tree.bottom_visible = True
-            else:
-                tree.bottom_visible = False
-        elif tree.bottom_visible == None:
-            print("bottom is None! checking bottom tree..")
-            self.check_tree(self.trees[tree.coordinates[0] + 1][tree.coordinates[1]])
-            if tree.height > tree.bottom_height:
-                #print statement that tree is visible from that direction
-                print("Tree at " + str(tree.coordinates) + " with height " + str(tree.height) + " is visible from the bottom")
-                tree.bottom_visible = True
-            else:
-                tree.bottom_visible = False
+                if self.trees[i+1][j].visible == True:
+                    print("if " + str(self.trees[i][j].height) + " > " + str(self.trees[i+1][j].height))
+                    if self.trees[i][j].height > self.trees[i+1][j].height:
+                        print("found right visible")
+                        self.trees[i][j].right_visible = True
+                    else:
+                        self.trees[i][j].right_visible = False
 
-    def set_visibility(self):
-        for i in range(self.rows):
-            for j in range(self.cols):
-                self.check_tree(self.trees[i][j])
+                if self.trees[i][j-1].visible == True:
+                    print("if " + str(self.trees[i][j].height) + " > " + str(self.trees[i][j-1].height))
+                    if self.trees[i][j].height > self.trees[i][j-1].height:
+                        print("found top visible")
+                        self.trees[i][j].top_visible = True
+                    else:
+                        self.trees[i][j].top_visible = False
+
+                if self.trees[i][j+1].visible == True:
+                    print("if " + str(self.trees[i][j].height) + " > " + str(self.trees[i][j+1].height))
+                    if self.trees[i][j].height > self.trees[i][j+1].height:
+                        print("found bottom visible")
+                        self.trees[i][j].bottom_visible = True
+                    else:
+                        self.trees[i][j].bottom_visible = False
+                
+                if self.trees[i][j].left_visible or self.trees[i][j].right_visible or self.trees[i][j].top_visible or self.trees[i][j].bottom_visible:
+                    self.trees[i][j].visible = True
+                else:
+                    self.trees[i][j].visible = False
 
 
     #function to check all trees and if they are visible from any of the 4 directions and print if they are visible from that direction
@@ -196,10 +176,13 @@ def create_forest(lines):
         for j in range(cols):
             trees[i].append(Tree(int(line[j]), (i, j), forest))
         i += 1
-    forest.set_forest(trees)
+    forest.set_trees(trees)
+    print(forest)
     forest.set_heights()
     forest.print_forest()
-    forest.set_visibility()
+    forest.check_tree()
+    print(forest)
+    
     return forest
 
 test_forest = create_forest(testlines)
